8cfd2af0e248d2f40405b9ec1f074cbff0119ded
Sam-Mobed <masdebom@gmail.com>
2023-07-05 20:52:47 -0400

    i have a disease

--- components/Home/Hero/index.tsx
+++ components/Home/Hero/index.tsx
@@ -32,3 +32,47 @@ export default function Hero() {
         </div>
     );
 }
+
+
+/* this was the code before i added framer motion
+
+import Image from 'next/image';
+import hafez from '../../images/hafez.png';
+import hill from '../../images/hill.png';
+import mountain from '../../images/mountain.png';
+
+const imageStyle = {
+    height: 'auto',
+    width: '100%'
+}
+
+export default function Hero() {
+    return(
+        <div className='w-full h-screen relative overflow-hidden'>
+            <Image 
+            src={hafez}
+            style={imageStyle}
+            alt="hafez-parallax"
+            className='absolute top-0'
+            />
+            <Image 
+            src={hill}
+            style={imageStyle}
+            alt="hill-parallax"
+            className='absolute bottom-0'
+            /> 
+            <Image
+            src={mountain}
+            style={imageStyle}
+            alt="mountain-parallax"
+            className='absolute bottom-0'
+            />
+        </div>
+    );
+}
+
+
+
+
+
+*/
