d4a2a5cad1218d141263e219c56674df55a413da
Sam-Mobed <masdebom@gmail.com>
2023-07-06 13:53:19 -0400

    parallax done

--- components/Home/Hero/index.tsx
+++ components/Home/Hero/index.tsx
@@ -16,35 +16,30 @@ const imageStyle = {
 export default function Hero() {
     const {scrollY} = useScroll();
 
-    const y1 = useTransform(scrollY, [0,300],[0,200]);
-    const y2 = useTransform(scrollY, [0,300],[0,-100]);
-    //const y1 = useTransform(scrollY, [0,300],[0,200]);
+    const hafez_y = useTransform(scrollY, [50,1000],[0,500]);
+    const mountain_y = useTransform(scrollY, [0,500],[7,300]);
+    const hill_y = useTransform(scrollY, [50,500],[0,100]);
     return(
         <div className='w-full h-screen relative overflow-hidden'>
+
+            <motion.div
+            className='mountain-container absolute top-0'
+            style={{y:hafez_y}}
+            >       
+
             <Image 
             src={hafez}
             style={imageStyle}
             alt="hafez-parallax"
             className='absolute top-0'
             />
-            <Image 
-            src={hill}
-            style={imageStyle}
-            alt="hill-parallax"
-            className='absolute bottom-0'
-            /> 
-            <Image
-            src={mountain}
-            style={imageStyle}
-            alt="mountain-parallax"
-            className='absolute bottom-0'
-            />
 
+            </motion.div>
             
 
             <motion.div
             className='mountain-container absolute bottom-0'
-            style={{y:y1,x:50}}
+            style={{y:mountain_y}}
             >
 
             <Image
@@ -54,6 +49,18 @@ export default function Hero() {
             className='absolute bottom-0'
             />
 
+            </motion.div>
+
+            <motion.div
+            className='mountain-container absolute bottom-0'
+            style={{y:hill_y}}
+            >
+            <Image 
+            src={hill}
+            style={imageStyle}
+            alt="hill-parallax"
+            className='absolute bottom-0'
+            /> 
             </motion.div>
             
         </div>
