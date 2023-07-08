0a297fb0462c76409a47111481ef6fa380f6b570
Sam-Mobed <masdebom@gmail.com>
2023-07-07 22:54:06 -0400

    a lot of messed animations

--- components/Home/Title/index.tsx
+++ components/Home/Title/index.tsx
@@ -46,7 +46,7 @@ export default function Title({title}: Props){
             <div className='relative'>
                 <DynamicElements />
 
-                <div className='w-screen bg-black' style={{height:'20vh'}}>
+                <div className='w-screen bg-black' style={{height:'25vh'}}>
 
                 </div>
 
@@ -56,7 +56,7 @@ export default function Title({title}: Props){
                     />
                 </div>
 
-                <div className='w-screen bg-black' style={{height:'15vh'}}>
+                <div className='w-screen bg-black' style={{height:'5vh'}}>
 
                 </div>
 
--- components/Home/Title/movingElements.tsx
+++ components/Home/Title/movingElements.tsx
@@ -11,59 +11,115 @@ import moving_plants_right from '../../images/moving-plants-right.png';
 import moving_plants_left from '../../images/moving-plants-left.png';
 
 const plantStyle = {
-    width: '20vw',
+    width: '22vw',
+    height: 'auto',
+}
+
+const cornerStyle = {
+    width: '27vw',
     height: 'auto',
 }
 
 const sunStyle = {
     width: 'auto',
-    height: '15vh',
+    height: 'calc(45%)',
+}
+
+/*
+const sunStyle = {
+    width: 'calc(20%)',
+    height: 'calc(20%)',
+}
+*/
+
+const simurghStyle = {
+    width: '35vw',
+    height: 'auto',
+}
+
+const cornerHillStyle = {
+    width: '30vw',
+    height: 'auto',
+    right: '0%',
+    bottom: '0%'
 }
 
 // these need to be floating elements.
 export default function DynamicElements(){
     const {scrollY} = useScroll();
 
-    const hafez_y = useTransform(scrollY, [50,1000],[0,500]);
-    const mountain_y = useTransform(scrollY, [0,500],[7,300]);
-    const hill_y = useTransform(scrollY, [50,500],[0,100]);
+    const sun_Y = useTransform(scrollY, [400,1000],[0,120]);
+
+    const left_plant_Y = useTransform(scrollY, [400,1000],[-100,0]);
+    const left_plant_X = useTransform(scrollY, [400,1000],[-300,0]);
+
+    const right_plant_Y = useTransform(scrollY, [400,1000],[100,0]);
+    const right_plant_X = useTransform(scrollY, [400,1000],[300,0]);
 
     return (
         <div>
             <div className='absolute top-0 w-screen flex flex-row justify-between'>
-                <Image 
-                src={moving_plants_left}
-                style={plantStyle}
-                alt="moving_plants_left"
-                />
-                <Image 
-                src={Sun}
-                style={sunStyle}
-                alt="sun"
-                />
-                <Image 
-                src={moving_plants_right}
-                style={plantStyle}
-                alt="moving_plants_right"
-                />
+                <motion.div
+                style={{
+                    y:left_plant_Y,
+                    x:left_plant_X
+                }}
+                >
+                    <Image 
+                    src={moving_plants_left}
+                    style={plantStyle}
+                    alt="moving_plants_left"
+                    className='object-contain'
+                    />
+                    
+                </motion.div>
+
+                <motion.div
+                style={{
+                    y:sun_Y,
+                    rotate:0
+                }}
+                >   
+                    <Image 
+                    src={Sun}
+                    style={sunStyle}
+                    alt="sun"
+                    />
+                </motion.div>
+
+                <motion.div
+                style={{
+                    y:right_plant_Y,
+                    x:right_plant_X
+                }}
+                >
+                    <Image 
+                    src={moving_plants_right}
+                    style={plantStyle}
+                    alt="moving_plants_right"
+                    />
+                    
+                </motion.div>
             </div>
-            <div className='absolute right-0' style={{top:'40%'}}>
+            <div className='absolute right-0' style={{top:'30%',right:'10%'}}>
                 <Image 
                 src={Simurgh}
-                style={plantStyle}
+                style={simurghStyle}
                 alt="simurgh"
                 />
             </div>
             <div className='absolute bottom-0 w-screen flex flex-row justify-between'>
                 <Image 
                 src={Beast}
-                style={plantStyle}
+                style={cornerStyle}
                 alt="Beast"
+                className='inline-block'
                 />
                 <Image 
                 src={moving_hill_right}
-                style={sunStyle}
+                style={cornerHillStyle}
                 alt="moving_hill_right"
+                className='inline-block'
                 />
             </div>
         </div>
