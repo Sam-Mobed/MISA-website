13528fd933b5eea211dbd375aea500b18927fd1e
Sam-Mobed <masdebom@gmail.com>
2023-07-24 14:35:32 -0400

    stagerbox progress

--- components/Home/Values/staggerbox.tsx
+++ components/Home/Values/staggerbox.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import '../../shared/style.css';
+import './staggerb.css';
 
-import Image from 'next/image';
 import { motion } from 'framer-motion';
 
 type Props = {
@@ -25,7 +25,79 @@ val3txt
 }:Props
 ){
     return (
-        <div className='flex flex-wrap justify-between justify-center'>
+        <div className="flex-container">
+            <div className="flex-item">
+                <h3 className='text-3xl md:text-3xl lg:text-4xl mb-3'>
+                    {val1}
+                </h3>
+                <div className='text-center text-paragraph item1'>
+                    {val1txt.split(' ').map((word, index)=>
+                    <motion.span
+                    key={index}
+                    initial = {{ opacity: 0}}
+                    whileInView = {{ opacity: 1}}
+                    transition = {{ duration: 0.8, delay: index * 0.17}}
+                    viewport={{ once: true }}
+                    >
+                        <span>
+                            {word}{' '}
+                        </span>
+                    </motion.span>
+                    )}
+                </div>
+            </div>
+            <div className="flex-item">
+                <h3 className='text-3xl md:text-3xl lg:text-4xl mb-3'>
+                    {val2}
+                </h3>
+                <div className='text-center text-paragraph item2'>
+                    {val2txt.split(' ').map((word, index)=>
+                    <motion.span
+                    key={index}
+                    initial = {{ opacity: 0}}
+                    whileInView = {{ opacity: 1}}
+                    transition = {{ duration: 0.8, delay: index * 0.17}}
+                    viewport={{ once: true }}
+                    >
+                        <span>
+                            {word}{' '}
+                        </span>
+                    </motion.span>
+                    )}
+                </div>
+            </div>
+            <div className="flex-item">
+                <h3 className='text-3xl md:text-3xl lg:text-4xl mb-3'>
+                    {val3}
+                </h3>
+                <div className='text-center text-paragraph item3'>
+                    {val3txt.split(' ').map((word, index)=>
+                    <motion.span
+                    key={index}
+                    initial = {{ opacity: 0}}
+                    whileInView = {{ opacity: 1}}
+                    transition = {{ duration: 0.8, delay: index * 0.17}}
+                    viewport={{ once: true }}
+                    >
+                        <span>
+                            {word}{' '}
+                        </span>
+                    </motion.span>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+/*
+<div className="flex-container">
+            <div className="flex-item">getMessagesOrError (webpack-internal:///(sc_server)/./node_modules/use-intl/dist/src/core/createBaseTranslator.js:65:27)</div>
+            <div className="flex-item">getMessagesOrError (webpack-internal:///(sc_server)/./node_modules/use-intl/dist/src/core/createBaseTranslator.js:65:27)</div>
+            <div className="flex-item">getMessagesOrError (webpack-internal:///(sc_server)/./node_modules/use-intl/dist/src/core/createBaseTranslator.js:65:27)</div>
+        </div>
+----------------
+<div className='flex flex-wrap justify-between justify-center'>
             <div className='w-1/2 md:w-1/4 lg:w-1/6 p-4 flex flex-col items-center'>
                 <h3 className='text-3xl md:text-3xl lg:text-4xl'>
                     {val1}
@@ -89,5 +161,5 @@ val3txt
                 </div>
             </div>
         </div>
-    );
-}
+
+*/
