29899c88db549d4e20a3dca77f5b6f795c64df26
Sam-Mobed <masdebom@gmail.com>
2023-07-25 15:38:52 -0400

    progress compass

--- components/Home/Discover/animated.tsx
+++ components/Home/Discover/animated.tsx
@@ -1,9 +1,37 @@
+"use client"
 
+import '../../shared/style.css';
 
-export default function Animated(){
+import {motion} from 'framer-motion';
+
+type Props = {
+    text:string;
+}
+
+const boxStyle = {
+    width: '80vw',
+    height: 'auto',
+    padding: '10px'
+}
+
+export default function Text(
+    {text}:Props
+){
     return (
-        <div>
-            
+        <div className='text-center text-paragraph' style={boxStyle}>
+            {text.split(' ').map((word, index)=>
+                <motion.span
+                key={index}
+                initial = {{ opacity: 0}}
+                whileInView = {{ opacity: 1}}
+                transition = {{ duration: 0.8, delay: index * 0.17}}
+                viewport={{ once: true }}
+                >
+                    <span>
+                        {word}{' '}
+                    </span>
+                </motion.span>
+            )}
         </div>
     );
 }
--- components/Home/Discover/index.tsx
+++ components/Home/Discover/index.tsx
@@ -1,7 +1,11 @@
+"use client"
+
 import '../../shared/style.css';
 
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from 'framer-motion';
+import Text from './animated';
 
 import Astrolab from '../../images/astrolab.png';
 import Woman from '../../images/Woman.png';
@@ -52,39 +56,85 @@ export default function Discover(
                     />
                 </div>
 
-                <div className='text-center text-paragraph'>
-                    {text}
-                </div>
-                
-                <div className="flex flex-row justify-center items-center text-paragraph m-3">
-                    <Link href="/about">
-                        {about}
-                    </Link>
-                </div>
+                <Text 
+                text={text}
+                />
 
-                <div className="flex flex-row justify-center items-center text-paragraph">
-                    <Link href="/events" className='m-3'>
-                        {events}
-                    </Link>
-                    <div>
-                        <Image
-                        src={Astrolab}
-                        style={astroStyle}
-                        alt="astrolab"
-                        className='m-3'
-                        />
+                <div className='flex flex-row justify-between items-center'>
+                    <div className='flex flex-col space-y-20 items-center'>
+                        <Link href="/about">
+                            {about}
+                        </Link>
+                        <Link href="/about#contact" >
+                            {contact}
+                        </Link>
                     </div>
-                    <Link href="/about#apply" className='m-3'>
-                        {involve}
-                    </Link>
-                </div>
 
-                <div className="flex flex-row justify-center items-center text-paragraph m-3">
-                    <Link href="/about#contact">
-                        {contact}
-                    </Link>
+                    <div className='justify-center center-items flex items-center'>
+                        <motion.div
+                        animate={{ rotate: 360 }}
+                        transition={{ duration:20, repeat: Infinity }}
+                        >
+                            <Image
+                            src={Astrolab}
+                            style={astroStyle}
+                            alt="astrolab"
+                            />
+                        </motion.div>
+                    </div>
+                    
+                    <div className='flex flex-col space-y-20 items-center'>
+                        <Link href="/events">
+                            {events}
+                        </Link>
+                        <Link href="/about#apply">
+                            {involve}
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
+
+/*
+<motion.div
+animate={{ y: [-10, 10, -10], transition: { repeat: Infinity, duration: 5 } }}
+>
+    <Link href="/about">
+        {about}
+    </Link>
+</motion.div>
+
+<div className="flex flex-row justify-center items-center text-paragraph">
+                    
+                    <Link href="/events" className='flex-1 flex justify-end'>
+                        {events}
+                    </Link>
+                    
+                    <motion.div
+                    animate={{ rotate: 360 }}
+                    transition={{ duration:20, repeat: Infinity }}
+                    
+                    >
+                        <Image
+                        src={Astrolab}
+                        style={astroStyle}
+                        alt="astrolab"
+                        />
+                    </motion.div>
+    
+                    <Link href="/about#apply" className='flex-1 flex justify-start'>
+                        {involve}
+                    </Link>
+                    
+                </div>
+
+                <div className="flex flex-row justify-center items-center text-paragraph m-3">
+                    
+                        <Link href="/about#contact">
+                            {contact}
+                        </Link>
+                    
+                </div>
+*/
