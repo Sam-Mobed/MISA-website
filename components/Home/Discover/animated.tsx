182490930467a541256f78b65c514b2020798a2e
Sam-Mobed <masdebom@gmail.com>
2023-07-25 05:08:52 -0400

    discover

---
+++ components/Home/Discover/animated.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Animated(){
+    return (
+        <div>
+            
+        </div>
+    );
+}
--- components/Home/Discover/index.tsx
+++ components/Home/Discover/index.tsx
@@ -1,11 +1,10 @@
-//"use client"
-
 import '../../shared/style.css';
 
 import Image from "next/image";
 import Link from "next/link";
 
 import Astrolab from '../../images/astrolab.png';
+import Woman from '../../images/Woman.png';
 
 type Props = {
     title:string;
@@ -16,9 +15,14 @@ type Props = {
     contact:string;
 }
 
-const imgStyle = {
-    width:'20vw',
+const astroStyle = {
+    width:'15vw',
     height:'auto'
+}
+
+const womanStyle = {
+    height: '15vh',
+    width: 'auto',
 }
 
 export default function Discover(
@@ -32,45 +36,54 @@ export default function Discover(
     }:Props
 ){
     return (
-        <div className="flex flex-col w-screen inline">
-            <div>
-                <h2 className='section-title text-3xl md:text-4xl lg:text-6xl'>
-                    {title.split(' ').map((word)=>
-                        <div>{word}</div>
-                    )}
-                </h2>
-            </div>
-
-            <div>
-                {text}
-            </div>
-            
-            <div className="flex flex-row justify-center items-center">
-                <Link href="/about">
-                    {about}
-                </Link>
-            </div>
-
-            <div className="flex flex-row justify-between justify-center items-center">
-                <Link href="/events">
-                    {events}
-                </Link>
-                <div>
+        <div className='w-screen inline flex justify-center items-center'>
+            <div className="flex flex-col" style={{width:'80vw'}}>
+                <div className='flex flex-row justify-between'>
+                    <h2 className='section-title text-3xl md:text-4xl lg:text-6xl'>
+                        {title.split(' ').map((word)=>
+                            <div>{word}</div>
+                        )}
+                    </h2>
+                    
                     <Image
-                    src={Astrolab}
-                    style={imgStyle}
+                    src={Woman}
+                    style={womanStyle}
                     alt="astrolab"
                     />
                 </div>
-                <Link href="/about#apply">
-                    {involve}
-                </Link>
-            </div>
 
-            <div className="flex flex-row justify-center items-center">
-                <Link href="/about#contact">
-                    {contact}
-                </Link>
+                <div className='text-center text-paragraph'>
+                    {text}
+                </div>
+                
+                <div className="flex flex-row justify-center items-center text-paragraph m-3">
+                    <Link href="/about">
+                        {about}
+                    </Link>
+                </div>
+
+                <div className="flex flex-row justify-center items-center text-paragraph">
+                    <Link href="/events" className='m-3'>
+                        {events}
+                    </Link>
+                    <div>
+                        <Image
+                        src={Astrolab}
+                        style={astroStyle}
+                        alt="astrolab"
+                        className='m-3'
+                        />
+                    </div>
+                    <Link href="/about#apply" className='m-3'>
+                        {involve}
+                    </Link>
+                </div>
+
+                <div className="flex flex-row justify-center items-center text-paragraph m-3">
+                    <Link href="/about#contact">
+                        {contact}
+                    </Link>
+                </div>
             </div>
         </div>
     );
