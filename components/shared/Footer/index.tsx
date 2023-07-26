b529cc041f108e474b1f2276681c0162ea434ee0
Sam-Mobed <masdebom@gmail.com>
2023-07-26 14:01:16 -0400

    translations

---
+++ components/shared/Footer/index.tsx
@@ -0,0 +1,16 @@
+import {FaInstagram} from "react-icons/fa";
+import {FaFacebook} from "react-icons/fa";
+import {FaTwitter} from "react-icons/fa";
+import {FaTelegram} from "react-icons/fa";
+
+type Props = {
+    
+}
+
+export default function Footer(){
+    return(
+        <div>
+
+        </div>
+    );
+}
