35b91757dcfa3ce5893aeb5afacbb3e3f27581e7
Sam-Mobed <masdebom@gmail.com>
2023-12-29 15:02:23 -0500

    adjusted flexbox of card-deck, now theres more space between them and a little more organized

--- components/About/Members/index.tsx
+++ components/About/Members/index.tsx
@@ -25,37 +25,41 @@ export default async function Members({title, text}:Props){
 
     return (
         <div className='w-screen inline flex justify-center items-center overflow-hidden' style={{backgroundColor:'bisque'}}>
-            <div className='flex flex-col' style={{width:'80vw'}}>
-                <div className='flex flex-row ml-5 mt-5 justify-between'>
-                    <h2 className='section-title text-3xl md:text-4xl lg:text-6xl'>
-                        {title.split(' ').map((word, index)=>
-                            <div key={`${index}-${word}`}>{word}</div>
-                        )}
-                    </h2>
-                    <Image 
-                    src={members}
-                    style={imgStyle}
-                    alt="historians"
-                    className='mr-8'
-                    />
-                </div>
+            <div className='flex flex-col justify-center items-center' style={{width:'95vw'}}>
+                <div className='flex flex-col' style={{width:'80vw'}}>
+                    <div className='flex flex-row ml-5 mt-5 justify-between'>
+                        <h2 className='section-title text-3xl md:text-4xl lg:text-6xl'>
+                            {title.split(' ').map((word, index)=>
+                                <div key={`${index}-${word}`}>{word}</div>
+                            )}
+                        </h2>
+                        <Image 
+                        src={members}
+                        style={imgStyle}
+                        alt="historians"
+                        className='mr-8'
+                        />
+                    </div>
 
-                <div className='text-paragraph flex justify-center mt-5'>
-                    {text}
+                    <div className='text-paragraph flex justify-center mt-5'>
+                        {text}
+                    </div>
                 </div>
                 
-                <div className='flex flex-row flex-wrap justify-between center-items'>
-                    {MISA_members.reverse().map((member,index)=>(
-                        <div className='flex-1 mb-5 mt-5 flex justify-center' style={{ flexBasis: '25%' }} key={`${index}-${member.name_en}`}>
-                            {locale === 'en' ? 
-                            <Card name={member.name_en} degreemajor={member.degree_major_en}  
-                            startfinish={member.start_finishDates} role={member.role_en}
-                            card_image={member.card_image} person_image={member.person_image} /> : 
-                            <Card name={member.name_fa} degreemajor={member.degree_major_fa}  
-                            startfinish={member.start_finishDates} role={member.role_fa}
-                            card_image={member.card_image} person_image={member.person_image} />}
-                        </div>
-                    ))}
+                <div className='w-full'>
+                    <div className='flex flex-row flex-wrap justify-between center-items'>
+                        {MISA_members.reverse().map((member,index)=>(
+                            <div className='flex-1 mb-3 mt-5 ml-1 mr-1 flex justify-center' style={{ flexBasis: '25%' }} key={`${index}-${member.name_en}`}>
+                                {locale === 'en' ? 
+                                <Card name={member.name_en} degreemajor={member.degree_major_en}  
+                                startfinish={member.start_finishDates} role={member.role_en}
+                                card_image={member.card_image} person_image={member.person_image} /> : 
+                                <Card name={member.name_fa} degreemajor={member.degree_major_fa}  
+                                startfinish={member.start_finishDates} role={member.role_fa}
+                                card_image={member.card_image} person_image={member.person_image} />}
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
