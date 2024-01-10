d071ce26246c38bc26e882c5a0505b59ed6a77be
Sam-Mobed <masdebom@gmail.com>
2024-01-09 22:50:29 -0500

    debugging loader

--- components/shared/LoadingCirc/index.tsx
+++ components/shared/LoadingCirc/index.tsx
@@ -3,7 +3,9 @@ import './loading.css';
 export default function LoadingCirc() {
     return (
         <div className="min-h-screen flex items-center justify-center">
-            <div className="loader-bars text-transparent">.</div>
+            
+            {/*<div className="loader-bars text-transparent">.</div>*/}
+            <div>Loading...</div>
         </div>
     );
 }
